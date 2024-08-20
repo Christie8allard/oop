504d0e12aec760edc22714f0574a9efbac4e92a1
Christie Ballard <christie.ballard@riotinto.com>
2024-08-20 20:31:36 +1000

    class

---
+++ Oop.py
@@ -0,0 +1,19 @@
+#class attribute
+class Dog(): #capital D makes it an object
+    pass
+    kind = 'canine'
+    breed = 'pug'
+    age = 5
+    colour = 'black'
+
+# instance attribute - things we want to change
+def __init__(self, breed, age, colour, name): #need to have self. included
+    self.name = name 
+    self.breed = breed
+    self.age = age
+    self.colour = colour
+
+dog = Dog('jett', 'pug', '5', 'black')
+print(dog.name, dog.breed, dog.age, dog.colour)
+dog2 = Dog('bub', 'poodle', '3', 'blond')
+print(dog2.name, dog2.breed, dog2.age, dog2.colour)
